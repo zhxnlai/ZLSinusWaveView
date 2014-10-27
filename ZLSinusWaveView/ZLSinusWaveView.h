69fd0356a10e04e058c2737dcd871f8632a6c92f
Zhixuan Lai <zhxnlai@gmail.com>
2014-10-26 23:32:33 -0700

    first commit

---
+++ ZLSinusWaveView/ZLSinusWaveView.h
@@ -0,0 +1,52 @@
+//
+//  SIAudioPlot.h
+//  EZAudioRecordExample
+//
+//  Created by Zhixuan Lai on 8/2/14.
+//  Copyright (c) 2014 Syed Haris Ali. All rights reserved.
+//
+
+#import "EZAudioPlot.h"
+
+@interface ZLSinusWaveView : EZAudioPlot
+
+/// The amplitude that is used when the incoming microphone amplitude is near zero. Setting a value greater 0 provides a more vivid visualization.
+@property (assign,nonatomic) float idleAmplitude;
+
+/// The phase of the sinus wave. Default: 0.
+@property (assign,nonatomic) float phase;
+
+/// The frequency of the sinus wave. The higher the value, the more sinus wave peaks you will have. Default: 1.5
+@property (assign,nonatomic) float frequency;
+
+/// The damping factor that is used to calm the wave down after a sound level peak. Default: 0.86
+@property (assign,nonatomic) float dampingFactor;
+
+/// The number of additional waves in the background. The more waves, to more CPU power is needed. Default: 4.
+@property (assign,nonatomic) float waves;
+
+/// The actual amplitude the view is visualizing. This amplitude is based on the microphone's amplitude
+@property (assign,nonatomic,readonly) float amplitude;
+
+/// The damped amplitude.
+@property (assign,nonatomic,readonly) float dampingAmplitude;
+
+/// The lines are joined stepwise, the more dense you draw, the more CPU power is used. Default: 5.
+@property (assign,nonatomic) float density;
+
+/// The phase shift that will be applied with each delivering of the microphone's value. A higher value will make the waves look more nervous. Default: -0.15.
+@property (assign,nonatomic) float phaseShift;
+
+/// The white value of the color to draw the waves with. Default: 1.0 (white).
+@property (assign,nonatomic) float whiteValue;
+
+/// Set to NO, if you want to stop the view to oscillate.
+@property (assign,nonatomic) BOOL oscillating;
+
+/*
+ * Color to use when drawing the waves
+ * Default: white
+ */
+@property (nonatomic, strong) UIColor *waveColor;
+
+@end
---
+++ ZLSinusWaveView/ZLSinusWaveView.m
@@ -0,0 +1,153 @@
+//
+//  SIAudioPlot.m
+//  EZAudioRecordExample
+//
+//  Created by Zhixuan Lai on 8/2/14.
+//  Copyright (c) 2014 Syed Haris Ali. All rights reserved.
+//
+
+#import "ZLSinusWaveView.h"
+
+@interface ZLSinusWaveView() {
+    int tick;
+}
+@end
+
+@implementation ZLSinusWaveView
+
+@synthesize backgroundColor = _backgroundColor;
+@synthesize color           = _color;
+@synthesize gain            = _gain;
+@synthesize plotType        = _plotType;
+@synthesize shouldFill      = _shouldFill;
+@synthesize shouldMirror    = _shouldMirror;
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+    }
+    return self;
+}
+
+-(void)awakeFromNib {
+    [super awakeFromNib];
+    [self setup];
+}
+
+-(void)setup {
+    _frequency = 1.5;
+    _phase = 0;
+    _amplitude = 1.0;
+    _whiteValue = 1.0;
+    _idleAmplitude = 0.1;
+    _dampingFactor = 0.86;
+    _waves = 5;
+    _phaseShift = -0.15;
+    _density = 5.0;
+}
+
+-(void)_refreshDisplay {
+#if TARGET_OS_IPHONE
+    [self setNeedsDisplay];
+#elif TARGET_OS_MAC
+    [self setNeedsDisplay:YES];
+#endif
+}
+
+-(void)setSampleData:(float *)data
+              length:(int)length {
+    
+    int requiredTickes = 1; // Alter this to draw more or less often
+    tick = (tick+1)%requiredTickes;
+    
+    // Let's use the buffer's first float value to determine the current sound level.
+    float value = fabsf(data[0]);
+    
+    /// If we defined the current sound level as the amplitude of the wave, the wave would jitter very nervously.
+    /// To avoid this, we use an inert amplitude that lifts slowly if the value is currently high, and damps itself
+    /// if the value decreases.
+    if (value > _dampingAmplitude) _dampingAmplitude += (fmin(value,1.0)-_dampingAmplitude)/4.0;
+    else if (value<0.01) _dampingAmplitude *= _dampingFactor;
+    
+    _phase += _phaseShift;
+    _amplitude = fmax( fmin(_dampingAmplitude*20, 1.0), _idleAmplitude);
+    
+    if (tick==0) {
+        if( plotData != nil ){
+            free(plotData);
+        }
+        
+        plotData   = (CGPoint *)calloc(sizeof(CGPoint),length);
+        plotLength = length;
+        
+        for(int i = 0; i < length; i++) {
+            data[i]     = i == 0 ? 0 : data[i];
+            plotData[i] = CGPointMake(i,data[i] * _gain);
+        }
+        
+        [self _refreshDisplay];
+    }
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(ctx);
+    CGRect frame = self.bounds;
+
+    // Set the background color
+    [(UIColor*)self.backgroundColor set];
+    UIRectFill(frame);
+    // Set the waveform line color
+    [(UIColor*)self.color set];
+
+    // NSLog(@"drawing %f waves", _waves);
+    
+    // We draw multiple sinus waves, with equal phases but altered amplitudes, multiplied by a parable function.
+    for(int i=0;i<_waves+1;i++) {
+        
+        // [[NSGraphicsContext currentContext] saveGraphicsState];
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        // CGContextSaveGState(context);
+        // CGContextRef context = (CGContextRef) [nsGraphicsContext graphicsPort];
+        
+        // The first wave is drawn with a 2px stroke width, all others a with 1px stroke width.
+        CGContextSetLineWidth(context, (i==0)? 2:1 );
+        
+        CGFloat halfHeight = CGRectGetHeight(self.bounds)/2;
+        CGFloat width = CGRectGetWidth(self.bounds);
+        CGFloat mid = width /2.0;
+        
+        const CGFloat maxAmplitude = halfHeight-4; // 4 corresponds to twice the stroke width
+        
+        // Progress is a value between 1.0 and -0.5, determined by the current wave idx, which is used to alter the wave's amplitude.
+        CGFloat progress = 1.0-(CGFloat)i/_waves;
+        CGFloat normedAmplitude = (1.5*progress-0.5)*_amplitude;
+        
+        // Choose the color based on the progress (that is, based on the wave idx)
+        // [[NSColor colorWithCalibratedWhite:_whiteValue alpha:progress/3.0*2+1.0/3.0] set];
+        // [[UIColor colorWithWhite:_whiteValue alpha:progress/3.0*2+1.0/3.0] set];
+        
+        CGFloat multiplier = MIN(1.0, (progress / 3.0f * 2.0f) + (1.0f / 3.0f));
+        [[[UIColor whiteColor] colorWithAlphaComponent:multiplier * CGColorGetAlpha([UIColor whiteColor].CGColor)] set];
+        
+        for(CGFloat x = 0; x<width+_density; x+=_density) {
+            // We use a parable to scale the sinus wave, that has its peak in the middle of the view.
+            CGFloat scaling = -pow(1/mid*(x-mid),2)+1;
+            CGFloat y = scaling *maxAmplitude *normedAmplitude *sinf(2 *M_PI *(x / width) *_frequency +_phase) + halfHeight;
+            
+            if (x==0) {
+                CGContextMoveToPoint(context, x, y);
+            }
+            else {
+                CGContextAddLineToPoint(context, x, y);
+            }
+        }
+        CGContextStrokePath(context);
+    }
+CGContextRestoreGState(ctx);
+}
+    
+@end
